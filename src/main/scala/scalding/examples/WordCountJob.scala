095a14989355107ccd3d90b80adeb1d3030196ea
Pranav Nair <pranavpnair@hotmail.com>
2014-05-23 17:32:25 +0530

    scalding assignment

--- src/main/scala/scalding/examples/WordCountJob.scala
+++ src/main/scala/scalding/examples/WordCountJob.scala
@@ -1,18 +1,21 @@
 package scalding.examples
 
 import com.twitter.scalding._
+import java.net.URL
 
 class WordCountJob(args : Args) extends Job(args) {
-  TextLine( args("input") )
+  TextLine(args("input"))
     .flatMap('line -> 'word) {
-      line : String => tokenize(line)
-    }
-    .groupBy('word) { _.size }
-    .write( Tsv( args("output") ) )
+    line: String => tokenize(line)
+  }
+    .groupBy('word){_.size }
+    .filter('size){size: Int => size > 4}
+    .write( Tsv(args("output")))
 
-  // split a piece of text into individual words
-  def tokenize(text : String) : Array[String] = {
-    text.toLowerCase.split(" ")
+  // split a piece of text into individual words and get the Top Level Domain
+  def tokenize(text: String) = Array[String] {
+    val host = new URL(text).getHost.split("\\.")
+    (host.reverse(1)+"."+host.reverse(0)).toString
   }
 }
 
@@ -20,9 +23,10 @@ object WordCountJob extends App {
   val progargs: Array[String] = List(
     "-Dmapred.map.tasks=200",
     "scalding.examples.WordCountJob",
-    "--input", "/tmp/files",
-    "--output", "/tmp/wordcount.txt",
+    "--input", "/home/aayush/scalding_examples/src/main/resources/sampleurls.txt",
+    "--output", "/home/aayush/scalding_examples/src/main/resources/wordcount",
     "--hdfs"
   ).toArray
   Tool.main(progargs)
 }
+
